fd1d880301514b04f36d47b0c87349a607ded63a
MalxMalx <alexandrmalka@gmail.com>
2018-10-11 17:42:14 +0300

    Error handling middleware + minor refactoring

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { port, apiPrefix, routes } = require('config');
+const { port, routes } = require('config');
 const echoPostHandler = require('./routes/echo/handler');
 const app = express();
 
 app.use(bodyParser.json());
 
-app.post(`${apiPrefix}${routes.echo}`, echoPostHandler);
+app.post(routes.echo, echoPostHandler);
+
+app.use((err, req, res, next) => {
+  console.log(err);
+
+  res.status(err.httpStatusCode);
+  if (err.data !== null && typeof err.data !== 'undefined') {
+    res.json(err.data);
+  }
+});
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
